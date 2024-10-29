f7a2fb0a5cb0b5c6ddeed32d25dbcaafb293126b
huylebron <doquochuy27102003@gmail.com>
2024-10-29 17:18:43 +0700

    bai 1 : delegate cơ bản

---
+++ Delegate_execise/Delegate Cơ Bản/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegate_execise.Delegate_Cơ_Bản ;
+delegate int MathOperation(int a, int b);
+public class Math
+{
+    protected static int add ( int a , int b ) => a + b ;
+   protected static int Sub ( int a , int b ) => a - b ;
+    protected static int Mul ( int a , int b ) => a * b ;
+   protected static int Div ( int a , int b ) => a / b ;
+
+    static void Main ( ) {
+        MathOperation Add = add ; 
+        MathOperation sub = Sub ;
+        MathOperation mul = Mul ;
+        MathOperation div = Div ;
+        Console.WriteLine($"Add: {Add(1,2)}");
+        Console.WriteLine($"Sub: {sub(1,2)}");
+        Console.WriteLine($"Mul: {mul(1,2)}");
+        Console.WriteLine($"Div: {div(1,2)}");
+        
+        
+
+
+    }
+
+
+
+}
