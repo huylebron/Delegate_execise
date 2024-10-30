b6eaec3414b536cabc7605d10e82aefbc315ed5e
huylebron <doquochuy27102003@gmail.com>
2024-10-30 16:56:30 +0700

    delegate bài 2

---
+++ Delegate_execise/Delegate với Func và Action/MathOperator.cs
@@ -0,0 +1,28 @@
+﻿namespace Delegate_execise.Delegate_với_Func_và_Action ;
+
+
+partial class MathOperator
+{
+
+    static int Add ( int a , int b ) => a + b ;
+    static int Sub ( int a , int b ) => a - b ; 
+    static int Mul ( int a , int b ) => a * b ;
+    static int Div ( int a , int b ) => a / b ;
+
+    static void Main ( ) {
+        Func < int , int , int > add = Add ;
+        Func < int , int , int > sub = Sub ;
+        Func < int , int , int > mul = Mul ;
+        Func < int , int , int > div = Div ;
+
+        Action < string > print = Console . WriteLine ; // delegate khoong trả về giá trị 
+        
+        print ( $"add : {add ( 1 , 2 )}" ) ;
+        print ( $"sub : {sub ( 1 , 2 )}" ) ;
+        print ( $"mul : {mul ( 1 , 2 )}" ) ;
+        print ( $"div : {div ( 1 , 2 )}" ) ;
+        
+
+    }
+    
+}
